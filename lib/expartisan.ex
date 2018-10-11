c106dd77a87de1ab028ea0454d5218e33e4766d2
Jacob Lerche <jacob.lerche@gmail.com>
2018-10-11 13:53:49 -0700

    Adds basic API wrap

--- lib/expartisan.ex
+++ lib/expartisan.ex
@@ -3,16 +3,39 @@ defmodule ExPartisan do
   Documentation for ExPartisan.
   """
 
-  @doc """
-  Hello world.
+  def join(%{} = node) do
+    :partisan_pluggable_peer_service_manager.join(node)
+  end
 
-  ## Examples
+  def members() do
+    :partisan_pluggable_peer_service_manager.members()
+  end
 
-      iex> ExPartisan.hello()
-      :world
+  def leave() do
+    :partisan_pluggable_peer_service_manager.leave()
+  end
 
-  """
-  def hello do
-    :world
+  def leave(node) do
+    :partisan_pluggable_peer_service_manager.leave(node)
+  end
+
+  def forward_message(name, channel, server_ref, message, options) do
+    :partisan_pluggable_peer_service_manager.forward_message(
+      name,
+      channel,
+      server_ref,
+      message,
+      options
+    )
+  end
+
+  def cast_message(name, channel, server_ref, message, options) do
+    :partisan_pluggable_peer_service_manager.cast_message(
+      name,
+      channel,
+      server_ref,
+      message,
+      options
+    )
   end
 end
