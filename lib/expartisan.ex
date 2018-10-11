39585b7bfc0f5b0a3e8f38a207a2df6ec5058159
Jacob Lerche <jacob.lerche@gmail.com>
2018-10-11 15:04:55 -0700

    function docs boilerplate

--- lib/expartisan.ex
+++ lib/expartisan.ex
@@ -7,36 +7,57 @@ defmodule ExPartisan do
   @type name :: atom | {:global, term} | {:via, module, term}
   @type server :: pid | name | {atom, node}
 
+  @doc """
+
+  """
   @spec myself() :: partisan_node
   def myself do
     :partisan_pluggable_peer_service_manager.myself()
   end
 
+  @doc """
+
+  """
   @spec join(node :: partisan_node) :: :ok
   def join(%{} = node) do
     :partisan_pluggable_peer_service_manager.join(node)
   end
 
+  @doc """
+
+  """
   @spec members() :: [partisan_node]
   def members() do
     :partisan_pluggable_peer_service_manager.members()
   end
 
+  @doc """
+
+  """
   @spec leave() :: :ok
   def leave() do
     :partisan_pluggable_peer_service_manager.leave()
   end
 
+  @doc """
+
+  """
   @spec leave(node :: partisan_node) :: :ok
   def leave(node) do
     :partisan_pluggable_peer_service_manager.leave(node)
   end
 
+  @doc """
+
+  """
   @spec forward_message(node :: partisan_node, server_ref :: server, message :: term) :: :ok
   def forward_message(node, server_ref, message) do
     forward_message(node, :undefined, server_ref, message, [{:transitive, true}])
   end
 
+  @doc """
+
+  """
   @spec cast_message(node :: partisan_node, server_ref :: server, message :: term) :: :ok
   def cast_message(node, server_ref, message) do
     cast_message(node, :undefined, server_ref, message, [{:transitive, true}])
